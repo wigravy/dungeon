7a2b5ef6944a6b72a01f86a71a4348afe5a16ce7
wigravy <wigravy@gmail.com>
2020-11-22 15:31:59 +0500

    lesson4

--- core/src/com/wigravy/dungeon/units/Monster.java
+++ core/src/com/wigravy/dungeon/units/Monster.java
@@ -1,17 +1,20 @@
 package com.wigravy.dungeon.units;
 
 import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.badlogic.gdx.math.MathUtils;
 import com.wigravy.dungeon.controllers.GameController;
 
 public class Monster extends Unit {
-    private float aiBrainsImplseTime;
+    private float aiBrainsImplsTime;
     private Unit target;
+    private float maxAggroRadius;
 
     public Monster(TextureAtlas atlas, GameController gc) {
         super(gc, 5, 2, 10);
         this.texture = atlas.findRegion("monster");
         this.textureHp = atlas.findRegion("hp");
         this.hp = -1;
+        this.maxAggroRadius = 5.0f;
     }
 
     public void activate(int cellX, int cellY) {
@@ -28,10 +31,10 @@ public class Monster extends Unit {
         super.update(dt);
         if (canIMakeAction()) {
             if (isStayStill()) {
-                aiBrainsImplseTime += dt;
+                aiBrainsImplsTime += dt;
             }
-            if (aiBrainsImplseTime > 0.4f) {
-                aiBrainsImplseTime = 0.0f;
+            if (aiBrainsImplsTime > 0.4f) {
+                aiBrainsImplsTime = 0.0f;
                 if (canIAttackThisTarget(target)) {
                     attack(target);
                 } else {
@@ -48,14 +51,24 @@ public class Monster extends Unit {
             for (int j = cellY - 1; j <= cellY + 1; j++) {
                 if (Math.abs(cellX - i) + Math.abs(cellY - j) == 1 && gc.getGameMap().isCellPassable(i, j) && gc.getUnitController().isCellFree(i, j)) {
                     float dst = (float) Math.sqrt((i - target.getCellX()) * (i - target.getCellX()) + (j - target.getCellY()) * (j - target.getCellY()));
-                    if (dst < bestDst) {
-                        bestDst = dst;
-                        bestX = i;
-                        bestY = j;
+                    if (dst <= maxAggroRadius) {
+                        if (dst < bestDst) {
+                            bestDst = dst;
+                            bestX = i;
+                            bestY = j;
+                        }
+                    } else {
+                        do {
+                            bestX = cellX + MathUtils.random(-1, 1);
+                            bestY = cellY + MathUtils.random(-1, 1);
+                        } while (Math.abs((bestX + bestY) - (cellX + cellY)) != 1);
+                        goTo(bestX, bestY);
+                        return;
                     }
                 }
             }
         }
+
         goTo(bestX, bestY);
     }
 }
